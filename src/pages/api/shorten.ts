4c183ec74ad363c78da1e7478f995ff00a682ba8
Ariaj Sarkar <rajsarkar02205@gmail.com>
2024-08-17 23:22:30 +0530

    small fix

--- src/pages/api/shorten.ts
+++ src/pages/api/shorten.ts
@@ -39,7 +39,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
         // Handle preflight requests (OPTIONS)
         if (req.method === 'OPTIONS') {
-            res.setHeader('Access-Control-Allow-Origin', req.headers.origin || ' ');
+            res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
             res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
             res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
             return res.status(200).end();
